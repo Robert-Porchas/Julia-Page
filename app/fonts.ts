71889a243da51f7d90f30936786b69146df0ec82
Robert Porchas <robertgeomaing@gmail.com>
2026-02-14 11:52:10 -0800

    feat:new file to allow usage globally for font

---
+++ app/fonts.ts
@@ -0,0 +1,7 @@
+import localFont from "next/font/local";
+
+export const bloklettersBalpen = localFont({
+  src: "../public/fonts/blokletters/Blokletters-Balpen.woff",
+  variable: "--font-blokletters-balpen",
+  display: "swap",
+});
